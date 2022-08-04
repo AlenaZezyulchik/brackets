2c1a3429e498a420d21ba74484497cc4a265a723
Aliaksandr Ziaziulchyk <aliaksandr_ziaziulchyk@epam.com>
2022-08-04 20:30:56 +0300

    feat: task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var bracketsConfig = { '[': ']', '(': ')', '<': '>', '{': '}' };
+
+    var result = str.split('').reduce(function(acc, curr) {
+
+        if (~Object.keys(bracketsConfig).indexOf(curr)) {
+            acc.push(curr)
+        }
+        if (~Object.values(bracketsConfig).indexOf(curr) && bracketsConfig[acc[acc.length - 1]] === curr) {
+            acc.length = --acc.length;
+        }
+        return acc;
+    }, []);
+    return !Boolean(result.length);
 }
